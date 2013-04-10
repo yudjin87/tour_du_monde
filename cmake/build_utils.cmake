3013f099c5fea68da9d438206417ed2ce411c02b
Eugene Chuguy <eugene.chuguy@gmail.com>
2013-04-10 18:58:11 +0200

    Script to build all configurations has been improved to specify build systems.

--- cmake/build_utils.cmake
+++ cmake/build_utils.cmake
@@ -1,17 +1,58 @@
 ###############################################################################
+# Generates build systems and build them using list of configurations and generator's
+# name.
+# __CONFIGURATIONS      - names of building configurations (debug, release_static);
+# __GENERATOR_NAME      - name of generators, e.g. "NMake Makefiles" or "Unix Makefiles";
+# __BUILD_TREE_PATH     - path where cmake will produce all its stuff including Makefiles;
+# __NATIVE_PARAMS   - pass remaining options to the native tool.
+# __PATH_TO_CMAKE_LISTS - path where CmakeLists.txt root config is;
+# Example:
+# crsl_build("debug;release" "Visual Studio 11" "../MSVC-x32" "-j4" ..)
+function(crsl_build __CONFIGURATIONS __GENERATOR_NAME __BUILD_TREE_PATH __NATIVE_PARAMS __PATH_TO_CMAKE_LISTS)
+  file(MAKE_DIRECTORY ${__BUILD_TREE_PATH})
+
+  ##############################################################################
+  # Generate a build system and perform building for all configurations.
+  foreach(__CONF ${__CONFIGURATIONS})
+    message(STATUS "Build type: " ${__CONF})
+
+    set(BUILD_RESULT 0)
+    crsl_generate_project(${__GENERATOR_NAME} ${__PATH_TO_CMAKE_LISTS} ${__CONF} ${__BUILD_TREE_PATH} "TRUE" BUILD_RESULT)
+  
+    if(BUILD_RESULT GREATER 0)
+      message(FATAL_ERROR "CMake configuration for generator '${__GENERATOR_NAME}' failed.")   
+      return()
+    endif()
+
+    crsl_build_project(${__CONF} ${__BUILD_TREE_PATH} "${__NATIVE_PARAMS}")
+  endforeach(__CONF)
+
+endfunction(crsl_build)
+
+###############################################################################
 # Generates a build system.
 # __GENERATOR_NAME  - name of generators, e.g. "NMake Makefiles" or "Unix Makefiles";
+# __PATH_TO_CMAKE_LISTS - path where CmakeLists.txt root config is;
 # __BUILD_TYPE      - name of building configuration (debug, release_static);
 # __BUILD_TREE_PATH - path where cmake will produce all its stuff including Makefiles;
 # __TEST            - ON: builds and runs tests, OFF: build only carousel library;
 # __BUILD_RESULT    - returns 0 if generating has finished succesfuly. Otherwise, return 1.
-function(crsl_generate_project __GENERATOR_NAME __BUILD_TYPE __BUILD_TREE_PATH __TEST __BUILD_RESULT )
+function(crsl_generate_project __GENERATOR_NAME __PATH_TO_CMAKE_LISTS __BUILD_TYPE __BUILD_TREE_PATH __TEST __BUILD_RESULT)
   set(BUILD_RESULT 0)
+
+  message(STATUS "Building system: " ${__GENERATOR_NAME})
+  message(STATUS "Buil tree path1: " ${__BUILD_TREE_PATH})
+  message(STATUS "Configuration: " ${__BUILD_TYPE})
+  message(STATUS "Path to root CmakeLists.txt: " ${__PATH_TO_CMAKE_LISTS})
+  
+  
   execute_process(COMMAND 
-    ${CMAKE_COMMAND} -G 
-	${__GENERATOR_NAME} ".." "-DCMAKE_BUILD_TYPE=${__BUILD_TYPE}"  "-DCRSL_TEST=${__TEST}" 
-	WORKING_DIRECTORY ${__BUILD_TREE_PATH} 
-	RESULT_VARIABLE BUILD_RESULT)
+    ${CMAKE_COMMAND} 
+      -G ${__GENERATOR_NAME} 
+      ${__PATH_TO_CMAKE_LISTS}
+      "-DCMAKE_BUILD_TYPE=${__BUILD_TYPE}"  "-DCRSL_TEST=${__TEST}" 
+	    WORKING_DIRECTORY ${__BUILD_TREE_PATH} 
+	    RESULT_VARIABLE BUILD_RESULT)
 
   set(${__BUILD_RESULT} ${BUILD_RESULT} PARENT_SCOPE)
 endfunction(crsl_generate_project)
@@ -24,6 +65,10 @@ endfunction(crsl_generate_project)
 function(crsl_build_project __BUILD_TYPE __BUILD_TREE_PATH __NATIVE_PARAMS)
   message(STATUS "Build type: " ${__BUILD_TYPE})
  
+  message(STATUS "Buil tree path: " ${__BUILD_TREE_PATH})
+  message(STATUS "Configuration: " ${__BUILD_TYPE})
+  message(STATUS "Native parameters: " ${__NATIVE_PARAMS})
+
   if ("${__NATIVE_PARAMS}" STREQUAL "")
     execute_process(COMMAND ${CMAKE_COMMAND}
       --build .  
